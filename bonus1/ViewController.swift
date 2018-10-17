a7894f3b11e68fccbcee0d46ccb5b316db90ce71
User09 <user09@user09de-imac.local>
2018-10-17 11:23:33 +0800

    Initial Commit

---
+++ bonus1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bonus1
+//
+//  Created by User09 on 2018/10/17.
+//  Copyright © 2018 user09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
